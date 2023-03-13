5d07251411dae5fa08c18140f0eea2b34a03e4f5
Destiny Arupi <destinyarupi@gmail.com>
2023-03-13 14:46:03 -0500

    Update Specials.js

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import "../components/styles/Specials.css"
-import SpecialsCard from './SpecialsCard'
+import React from 'react';
+import "../components/styles/Specials.css";
+import SpecialsCard from './SpecialsCard';
 
-const Specials = () => {
-  return (
-    <div className="specials-section-container">
-      <div className="specials-section-title">
-            <h1>This Week's Specials</h1>
-            <div className="btn-container">
-                <button className="btn btn-menu">Online Menu</button>
-            </div>
+const Specials = () => (
+  <div className="specials-section-container">
+    <div className="specials-section-title">
+      <h1>This Week's Specials</h1>
+      <div className="btn-container">
+        <button className="btn btn-menu">Online Menu</button>
       </div>
-      <SpecialsCard />
     </div>
-  )
-}
+    <SpecialsCard />
+  </div>
+);
 
-export default Specials
+export default Specials;
