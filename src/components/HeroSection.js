d0aa3dae4a3ef7e67ff0723f69d454a3294a1a07
Destiny Arupi <destinyarupi@gmail.com>
2023-03-13 14:25:48 -0500

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import "../components/styles/HeroSection.css";
-import { NavLink } from 'react-router-dom';
-import HeroImage from "../components/assets/gourmet.jpg"
 
-const HeroSection = () => {
+import { NavLink } from 'react-router-dom';
+import '../components/styles/HeroSection.css';
+import heroImage from '../components/assets/gourmet.jpg';
+
+function HeroSection() {
+  const restaurantInfo = {
+    name: 'Little Lemon',
+    location: 'Chicago',
+    description:
+      'We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.',
+  };
+
   return (
-    <div className="hero-section-background">
-        <div className="hero-section-container">
-            <div className="section-left">
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-                <NavLink to="/reservations"><button className="btn">Reserve a Table</button></NavLink>
-            </div>
-            <div className="section-right">
-                <div className="image-box">
-                    <img src={HeroImage} alt="Serving delicious dish" />
-                </div>
-            </div>
+    <div className='hero-section-background'>
+      <div className='hero-section-container'>
+        <div className='section-left'>
+          <h1>{restaurantInfo.name}</h1>
+          <h2>{restaurantInfo.location}</h2>
+          <p>{restaurantInfo.description}</p>
+          <NavLink to="/reservations">
+            <button className={heroBackground.btn}>Reserve a Table</button>
+          </NavLink>
         </div>
+        <div className='section-right'>
+          <div className='image-box'>
+            <img src={heroImage} alt="Serving delicious dish" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
