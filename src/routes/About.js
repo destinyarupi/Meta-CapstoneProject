c1a29ceee392e0393c11e61bfc20d44f1cde8f4f
Destiny Arupi <destinyarupi@gmail.com>
2023-03-13 13:46:26 -0500

    Update About.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Header from '../components/Header'
 
 const About = () => {
+  const styles = {
+    textAlign: "center",
+    paddingTop: "15%"
+  }
   return (
     <div>
       <Header />
-      <h1 style={{textAlign: "center", paddingTop: "15%"}}>About</h1>
+      <h1 style={styles}>About</h1>
     </div>
   )
 }
