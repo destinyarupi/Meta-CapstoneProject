d9f12512338072d57053333c8e170421a0142797
Destiny Arupi <destinyarupi@gmail.com>
2023-03-13 14:34:09 -0500

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,56 +1,48 @@
-import React from 'react'
 
-export const Nav = () => {
+import React from 'react';
+import VectorImage from '../assets/images/Vector.png';
+
+const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container">
+      <div className="container">
         <a className="navbar-brand me-2" href="https://mdbgo.com/">
-        <img
-            src={require('../assets/images/Vector.png')}
-            alt="MDB Logo"
-            loading="lazy"
-            style={{marginTop: "-1px"}}
-        />
+          <img src={VectorImage} alt="MDB Logo" loading="lazy" style={{ marginTop: "-1px" }} />
         </a>
 
-        <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarButtonsExample"
-            aria-controls="navbarButtonsExample"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
-            <i className="fas fa-bars"></i>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarButtonsExample" aria-controls="navbarButtonsExample" aria-expanded="false" aria-label="Toggle navigation">
+          <i className="fas fa-bars"></i>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarButtonsExample">
-        <ul className="navbar-nav mx-auto mb-2 mb-lg-0 fw-bolder">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0 fw-bolder">
             <li className="nav-item">
-                <a className="nav-link" href="#">HOME</a>
+              <a className="nav-link" href="#">HOME</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">ABOUT</a>
+              <a className="nav-link" href="#">ABOUT</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">MENU</a>
+              <a className="nav-link" href="#">MENU</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">RESERVATION</a>
+              <a className="nav-link" href="#">RESERVATION</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">ORDER ONLINE</a>
+              <a className="nav-link" href="#">ORDER ONLINE</a>
             </li>
-        </ul>
+          </ul>
 
-        <div className="d-flex align-items-center fw-bolder">
+          <div className="d-flex align-items-center fw-bolder">
             <button type="button" className="btn px-3 me-2">
-                LOGIN
+              LOGIN
             </button>
+          </div>
         </div>
-        </div>
-    </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
+
+export { Nav };
+
