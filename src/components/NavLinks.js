69630642cfebe13e537feff195234cfbacff4d33
Destiny Arupi <destinyarupi@gmail.com>
2023-03-13 14:37:52 -0500

    Update NavLinks.js

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,72 +1,32 @@
+
 import React from 'react';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
 
+const NavLinks = ({ isMobile, closeMobileMenu }) => (
+  <motion.ul className="menu-items" initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }}>
+    <motion.li initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.05 }} onClick={() => isMobile && closeMobileMenu()}>
+      <NavLink to="/" className="active-links">Home</NavLink>
+    </motion.li>
+    <motion.li initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.10 }} onClick={() => isMobile && closeMobileMenu()}>
+      <NavLink to="#">About</NavLink>
+    </motion.li>
+    <motion.li initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.15 }} onClick={() => isMobile && closeMobileMenu()}>
+      <NavLink to="#">Menu</NavLink>
+    </motion.li>
+    <motion.li initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.20 }} onClick={() => isMobile && closeMobileMenu()}>
+      <NavLink to="/reservations" className="active-links">Reservations</NavLink>
+    </motion.li>
+    <motion.li initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.25 }} onClick={() => isMobile && closeMobileMenu()}>
+      <NavLink to="#">Order Online</NavLink>
+    </motion.li>
+    <motion.li initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.30 }} onClick={() => isMobile && closeMobileMenu()}>
+      <NavLink to="#">Login</NavLink>
+    </motion.li>
+    <motion.li initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.35 }} onClick={() => isMobile && closeMobileMenu()} className="donate">
+      <NavLink to="#">Donate</NavLink>
+    </motion.li>
+  </motion.ul>
+);
 
-const NavLinks = (props) => {
-  const animateFrom = {opacity: 0, y: -40}
-  const animateTo = {opacity: 1, y: 0}
-  return (
-    <motion.ul className="menu-items" initial={animateFrom} animate={animateTo} transition={{delay: 0.05}}>
-            <motion.li
-              initial={animateFrom}
-              animate={animateTo}
-              transition={{delay: 0.05}}
-              onClick={() => props.isMobile && props.closeMobileMenu()}
-              >
-                <NavLink to="/" className="active-links">Home</NavLink>
-              </motion.li>
-            <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{delay: 0.10}}
-            onClick={() => props.isMobile && props.closeMobileMenu()}
-            >
-              <NavLink to="#">About</NavLink>
-            </motion.li>
-            <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{delay: 0.15}}
-            onClick={() => props.isMobile && props.closeMobileMenu()}
-            >
-              <NavLink to="#">Menu</NavLink>
-            </motion.li>
-            <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{delay: 0.20}}
-            onClick={() => props.isMobile && props.closeMobileMenu()}
-            >
-              <NavLink to="/reservations" className="active-links">Reservations</NavLink>
-            </motion.li>
-            <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{delay: 0.25}}
-            onClick={() => props.isMobile && props.closeMobileMenu()}
-            >
-              <NavLink to="#">Order Online</NavLink>
-            </motion.li>
-            <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{delay: 0.30}}
-            onClick={() => props.isMobile && props.closeMobileMenu()}
-            >
-              <NavLink to="#">Login</NavLink>
-            </motion.li>
-            <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{delay: 0.35}}
-            onClick={() => props.isMobile && props.closeMobileMenu()}
-            className="donate"
-            >
-              <NavLink to="#">Donate</NavLink>
-            </motion.li>
-        </motion.ul>
-  )
-}
-
-export default NavLinks
+export default NavLinks;
