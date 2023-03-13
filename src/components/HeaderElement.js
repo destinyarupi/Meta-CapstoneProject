c4144af25a6e57fe206a179b4ec6a8308efb42a3
Destiny Arupi <destinyarupi@gmail.com>
2023-03-13 14:20:56 -0500

    Update HeaderElement.js

--- src/components/HeaderElement.js
+++ src/components/HeaderElement.js
@@ -5,19 +5,25 @@ import Navigation from './Navigation';
 import { NavLink } from 'react-router-dom';
 import MobileNav from './MobileNav';
 
-const Navbar = () => {
+function Navbar() {
+  const headerWrapperClasses = "header-wrapper";
+  const imageWrapperClasses = "image-wrapper";
+  const logoImgClasses = "logo-img";
+  const altText = "Little Lemon logo";
 
   return (
     <div className="header">
-      <div className="header-wrapper">
-        <div className="image-wrapper">
-          <NavLink to="/">{<img className="logo-img" src = { Logo } alt = "Little Lemon logo" />}</NavLink>
+      <div className={headerWrapperClasses}>
+        <div className={imageWrapperClasses}>
+          <NavLink to="/">
+            <img className={logoImgClasses} src={Logo} alt={altText} />
+          </NavLink>
         </div>
         <Navigation />
         <MobileNav />
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
